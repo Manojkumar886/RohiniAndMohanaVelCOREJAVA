6db97ee923f6192f09b960a7f59a7b5b2fe2c828
Manojkumar886 <maddymano434@gmail.com>
2023-03-22 13:01:12 +0530

    Variable is started

---
+++ src/main/java/FundamentalJava/Variables/LocalVariable.java
@@ -0,0 +1,25 @@
+package FundamentalJava.Variables;
+
+public class LocalVariable
+{
+    public static void main(String[] args)
+    {
+        //object creation
+//        classname objname=new classname();
+        LocalVariable obj1=new LocalVariable();//accessed by local variable or user defined method
+        obj1.localvariable();//calling a method
+        obj1.lcv1();
+    }
+
+    public void localvariable()
+    {
+        String AccoundHolder="Manojkumar";//local variable
+        System.out.println("My indian bank name is :"+AccoundHolder);
+    }
+
+    public void lcv1()
+    {
+        double Balane=89000.0;
+        System.out.println("your bank balance is :"+Balane);
+    }
+}
