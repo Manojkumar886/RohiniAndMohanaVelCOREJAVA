0328c922f5ddb60f668a7a66dad8ce9c8f7d11ad
Manojkumar886 <maddymano434@gmail.com>
2023-05-03 13:02:25 +0530

    Polymorphism and Abstraction is over

---
+++ src/main/java/FundamentalJava/Polymorphism/DemoOverriding3.java
@@ -0,0 +1,37 @@
+package FundamentalJava.Polymorphism;
+
+public class DemoOverriding3 extends  DemoOverriding2
+{
+//    upcasting=object created by parent class ,referenced by child class.
+    public int Calculation(int AValue,int Bvalue)
+    {
+        int divition=AValue*Bvalue;
+        return divition;
+    }
+    public int Calculation(float Avalue,int Bvalue)
+    {
+        int ConvertAvalue=(int)Avalue;//using narrowing typecasting
+
+        int divition=ConvertAvalue/Bvalue;
+        return divition;
+    }
+
+    public String Connection()
+    {
+        return "Manojkumar";
+    }
+
+    public static void main(String[] args)
+    {
+        DemoOverRiding1 ride=new DemoOverriding2();//upcasting
+        System.out.println(ride.Calculation(35,10));//350
+
+        DemoOverRiding1 ride1=new DemoOverriding3();
+        System.out.println(ride1.Calculation(10,10));//100
+
+
+    }
+
+
+
+}
