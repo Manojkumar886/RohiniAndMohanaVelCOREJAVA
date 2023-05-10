eb25a60a9d3db473d1867893a9edb282eee4c821
Manojkumar886 <maddymano434@gmail.com>
2023-05-10 13:02:02 +0530

    Add and list -console part 1 is done

--- src/main/java/FundamentalJava/ConsoleApplication/Executionofstudentdetail.java
+++ src/main/java/FundamentalJava/ConsoleApplication/Executionofstudentdetail.java
@@ -1,24 +1,47 @@
 package FundamentalJava.ConsoleApplication;
 
+import java.util.Scanner;
+
 public class Executionofstudentdetail  implements ZealousCustomerDetailsPerforamance
 {
-    ZealousCustomerDetails [] studentdetails=new ZealousCustomerDetails[10];//Array Declaration
+    ZealousCustomerDetails [] studentdetails=new   ZealousCustomerDetails[10];
+//        {
+//            System.out.println(studentdetails[i]);
+//        }ZealousCustomerDetails[10];//Array Declaration
 
     public Executionofstudentdetail()//Default constuctor
     {
         studentdetails[0]=new ZealousCustomerDetails("Manojkumar",1.30,9789355930l,"Java Full Stack developer",28000.0f,"Razak");
         studentdetails[1]=new ZealousCustomerDetails("Saipavi",1.00,782798788722l,"Python Stack dveeloper",20000.0f,"Razak");
-        for(int i=0;i<studentdetails.length;i++)
-        {
-            System.out.println(studentdetails[i]);
-        }
     }
 
 
     public static void main(String[] args)
     {
 
-        Executionofstudentdetail execution=new Executionofstudentdetail();
+        Executionofstudentdetail execution=new Executionofstudentdetail();//Constructor
+        Scanner scan=new Scanner(System.in);
+        do
+        {
+            System.out.println("Welcome to Zealous Academy");
+            System.out.println("which process you want work this"+"\n1.AddnewStudentdetails\n2.Listallstudentdetails\n3.updatestudentdetails\n4.deletecustomerdetail\n5.sortingstudentdetails");
+            int usage=scan.nextInt();
+            switch (usage)
+            {
+                case 1:
+                    System.out.println("adding new student details"+"\nStudentname\tHours\tStudentMobileno\tCoursedetail\tprice\tInchangeName");
+                    ZealousCustomerDetails customer1=new ZealousCustomerDetails(scan.next(), scan.nextDouble(), scan.nextLong(),scan.next(), scan.nextFloat(), scan.next());
+                    execution.addcustomerdetail(customer1);
+                    break;
+                case 2:
+                    System.out.println("List all you values ");
+                    execution.listallcostomerdetails();
+
+            }
+
+
+        }
+        while (true);
 
 
     }
@@ -26,13 +49,25 @@ public class Executionofstudentdetail  implements ZealousCustomerDetailsPerforam
     @Override
     public String addcustomerdetail(ZealousCustomerDetails zealous)
     {
-
-        return null;
+        for(int pos=0;pos<studentdetails.length;pos++)
+        {
+            if(studentdetails[pos]==null)
+            {
+                studentdetails[pos]=zealous;
+                System.out.println(zealous.getStudentName()+" has been added in our studentdetails");
+                break;
+            }
+        }
+        return zealous.getStudentName()+" has not been added ";
     }
 
     @Override
     public void listallcostomerdetails()
     {
+        for(ZealousCustomerDetails customers:studentdetails)
+        {
+            System.out.println(customers);
+        }
 
     }
 
